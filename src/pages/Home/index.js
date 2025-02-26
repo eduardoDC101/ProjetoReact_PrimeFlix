4343e63670c800d8a5a9b0060594eb6d07e3e9d6
Eduardo Dourado <edutnb2018@gmail.com>
2025-02-26 17:26:58 -0300

    Modificações no JS do Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 export default function Home() {
   const [filmes, setFilmes] = useState([]);
@@ -15,15 +17,29 @@ export default function Home() {
         },
       });
 
-      console.log(resposta.data.results);
+      //console.log(resposta.data.results.slice(0, 10));
+      setFilmes(resposta.data.results.slice(0, 10));
     }
 
     loadFilmes();
   }, []);
 
   return (
-    <div>
-      <h1> BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
