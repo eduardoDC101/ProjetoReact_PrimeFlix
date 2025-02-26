f73046a52345579f1cbbe453eedbbfcf4a2559a8
Eduardo Dourado <edutnb2018@gmail.com>
2025-02-26 13:38:15 -0300

    Lapidando código

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,3 @@ const api = axios.create({
 });
 
 export default api;
-
-//https://api.themoviedb.org/3/movie/now_playing?api_key=0b6e6c26ce311127775a7eb18f01f290&language=pt-BR
-
-//URL BASE: https://api.themoviedb.org/3/
-
-//CHAVE: 0b6e6c26ce311127775a7eb18f01f290
