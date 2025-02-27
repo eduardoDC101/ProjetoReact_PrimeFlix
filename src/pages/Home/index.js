da19f1b9b79c2fb4408f03c471e51720d2d47aa9
Eduardo Dourado <edutnb2018@gmail.com>
2025-02-27 10:12:07 -0300

    Criando o Loading

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import "./home.css";
 
 export default function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -19,10 +20,19 @@ export default function Home() {
 
       //console.log(resposta.data.results.slice(0, 10));
       setFilmes(resposta.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando...</h2>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
