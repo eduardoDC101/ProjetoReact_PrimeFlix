7f293f8895a6795a722621190233076363dc13af
Eduardo Dourado <edutnb2018@gmail.com>
2025-02-28 10:30:03 -0300

    Criando os detalhes do filme em JS

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
 
 export default function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "0b6e6c26ce311127775a7eb18f01f290",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Filme não encontrado");
+          setLoading(false);
+        });
+    }
+
+    loadFilme();
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1> VER DETALHES DOS FILMES</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avalição: {filme.vote_average} /10</strong>
     </div>
   );
 }
